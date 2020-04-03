21d6fae84e976fa6fbebf831cd92e289a63601c4
danie <danie@LAPTOP-OAESQFA4.example.org>
2020-04-03 08:59:47 -0600

    Card object is complete I think.

--- CandyLand/src/objects/cards.java
+++ CandyLand/src/objects/cards.java
@@ -2,4 +2,29 @@ package objects;
 
 public class cards {
 //Danielle
+	
+	private String name;
+	private int spaceCount;
+	
+	public cards(String n,int sC )
+	{
+		name = n;
+		spaceCount = sC;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSpaceCount() {
+		return spaceCount;
+	}
+
+	public void setSpaceCount(int spaceCount) {
+		this.spaceCount = spaceCount;
+	}
 }
