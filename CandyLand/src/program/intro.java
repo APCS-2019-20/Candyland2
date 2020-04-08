cdb5320944051d5b2f1682f9e2e764301d6adf53
dung <dung@dungs-mbp.domain>
2020-04-08 16:06:43 -0600

    intro

--- CandyLand/src/program/intro.java
+++ CandyLand/src/program/intro.java
@@ -1,5 +1,42 @@
 package program;
 
+import java.util.Scanner;
+
 public class intro {
 //Michael
+	public static void greeting()
+	{
+		Scanner userInput = new Scanner (System.in);
+		Scanner userInput1 = new Scanner (System.in);
+		Scanner userInput2 = new Scanner (System.in);
+		
+		System.out.print("Hello, Welcome to Candyland.");
+		System.out.println("What is your name?");
+		
+		String name = userInput1.nextLine();
+		
+		System.out.println("Hello " + name + ". Welcome to Candyland.");
+		
+		System.out.println("Would you like the rules?");
+		
+		String rules = userInput2.nextLine();
+		if( rules == "yes")
+		{
+			System.out.println("Here are the rules");
+			
+			String ready = userInput.nextLine();
+			
+			if (ready == "ready")
+			{
+				System.out.println("Ok, let's get start");
+			}
+		
+		}
+	if(rules == "no" )
+		{
+			System.out.println("Ok, let's get started");
+		}
+	
 }
+	}
+
