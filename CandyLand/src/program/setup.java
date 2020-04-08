3ac592b6a8b7de55e83ab075f8198c13d3564cce
dung <dung@dungs-mbp.domain>
2020-04-08 17:25:58 -0600

    new setup

--- CandyLand/src/program/setup.java
+++ CandyLand/src/program/setup.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 import objects.Character;
 import objects.Tile;
 import objects.Card;
-public class setup {
+public class setup
+{
 // Michael
 	
 	public static void board() 
@@ -20,44 +21,44 @@ public class setup {
 		random  =(int) Math.random()*6;	
 		if(j%50 ==0)
 		{
-			data.Board[0] = new Tile(data.colors[random], "",-1, true, false);	
+			data.board[0] = new Tile(data.colors[random], "",-1, true, false);	
 		}
 		else if(j%55 ==0)
 		{
-			data.Board[0] = new Tile(data.colors[random], "",-1, false, true);	
+			data.board[0] = new Tile(data.colors[random], "",-1, false, true);	
 		}
 		else if(j == 4 )
 		{
-			data.Board[0] = new Tile(data.colors[random], "",59, false, false);	
+			data.board[0] = new Tile(data.colors[random], "",59, false, false);	
 		}
 		 if(j == 9 )
 		{
-			data.Board[0] = new Tile(data.colors[random], "Plumpy",-1, false, false);	
+			data.board[0] = new Tile(data.colors[random], "Plumpy",-1, false, false);	
 		}
 		 if(j == 18 )
 			{
-				data.Board[0] = new Tile(data.colors[random], "Candy Cane Forrest",-1, false, false);	
+				data.board[0] = new Tile(data.colors[random], "Candy Cane Forrest",-1, false, false);	
 			}
 		 if(j == 43 )
 			{
-				data.Board[0] = new Tile(data.colors[random], "Jolly",-1, false, false);	
+				data.board[0] = new Tile(data.colors[random], "Jolly",-1, false, false);	
 			}
 		 if(j == 75 )
 			{
-				data.Board[0] = new Tile(data.colors[random], "Gramma Nutt",-1, false, false);	
+				data.board[0] = new Tile(data.colors[random], "Gramma Nutt",-1, false, false);	
 			}
 		 if(j == 96 )
 			{
-				data.Board[0] = new Tile(data.colors[random], " Princess Lolly",-1, false, false);	
+				data.board[0] = new Tile(data.colors[random], " Princess Lolly",-1, false, false);	
 			}
 		 if(j == 104 )
 			{
-				data.Board[0] = new Tile(data.colors[random], "Ice Cream Sea",-1, false, false);	
+				data.board[0] = new Tile(data.colors[random], "Ice Cream Sea",-1, false, false);	
 			}
 		
 		else
 		{
-			data.Board[0] = new Tile(data.colors[random], "",-1, false, false);
+			data.board[0] = new Tile(data.colors[random], "",-1, false, false);
 		}
 	}
 	for(int k = 0; k < 61; k++)
@@ -65,34 +66,34 @@ public class setup {
 		random  =(int) Math.random()*6;	
 		if(k %18 ==0)
 		{
-			data.Cards[k] = new Card(data.colors[random], 1);
+			data.CARDS[k] = new Card(data.colors[random], 1);
 		}
 		else
 		{
-			data.Cards[k] = new Card(data.colors[random], 0);
+			data.CARDS[k] = new Card(data.colors[random], 0);
 		}
 		
 	}
+	}
 
-public class setup 
-{
 
 	public static void startup()
 	{
-		Scanner userInput = new Scanner (System.in);
-		Scanner userInput1 = new Scanner (System.in);
-		Scanner userInput2 = new Scanner (System.in);
-		Scanner userInput3 = new Scanner (System.in);
+	//	Scanner userInput = new Scanner (System.in);
+	//	Scanner userInput1 = new Scanner (System.in);
+	//	Scanner userInput2 = new Scanner (System.in);
+	//	Scanner userInput3 = new Scanner (System.in);
 		
-		int birth1 = userInput.nextInt();
-		int birth2 = userInput1.nextInt();
-		int birth3 = userInput2.nextInt();
-		int birth4 = userInput3.nextInt();
+	//	int birth1 = userInput.nextInt();
+	//	int birth2 = userInput1.nextInt();
+	//	int birth3 = userInput2.nextInt();
+	//	int birth4 = userInput3.nextInt();
 		
-		data.players.add(new Character("Gingerbread man",  false, false ));
-		data.players.add(new Character("CandyCane",  false, false ));
-		data.players.add(new Character("Gumdrop",  false, false ));
-		data.players.add(new Character("Icecream Cone",  false, false ));
+		data.players.add(new Character("Gingerbread man",0 , false, false ));
+		data.players.add(new Character("CandyCane",0 , false, false ));
+		data.players.add(new Character("Gumdrop",0 , false, false ));
+		data.players.add(new Character("Icecream Cone",0 , false, false ));
 
 	}
 }
+	
